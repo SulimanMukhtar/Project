220af82af7ce44c3d5b98f265cf1aae0ed81e983
qamer-del <55939365+qamer-del@users.noreply.github.com>
2021-07-09 08:06:51 -0700

    Update Index.php

--- Index.php
+++ Index.php
@@ -3,15 +3,6 @@
 ?>
 <!DOCTYPE html>
 <html>
-<<<<<<< HEAD
-        <head>
-            <title>F*ck</title>
-            <script></script>
-        </head>
-       <body>
-           <h1>HI fk you</h1>
-       </body> 
-=======
     <head>
         <title>F*ck</title>
         <script></script>
@@ -19,5 +10,4 @@
     <body>
         
     </body>
->>>>>>> 48e359b28fd85f2e44be928c518387e8c53da5bd
 </html>
